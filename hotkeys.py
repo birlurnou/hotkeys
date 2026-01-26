aae22c4589e6c816d7860b5f1eb348f3e0ff26ae
birlurnou <birlurnou@gmail.com>
2026-01-26 17:34:18 +0500

    Big rework

--- hotkeys.py
+++ hotkeys.py
@@ -5,79 +5,103 @@ import os
 import subprocess
 import find_browsers as fb
 
-'''
-
-формат словаря
-
-"key": "hotkey",
-"type": "url/program",
-"url": "ссылка на сайт/программу",
-"args": "для url: msedge, chrome, ...; для program: '-windowed', '-fullscreen', ..."
-
-пример data.json в репозитории
-
-hotkey клавиши
-
-(space, enter, ctrl, shift, alt, windows, letters)
-например, shift + space + s
-
-'''
-
+# чтение данных из json
 with open('data.json', 'r', encoding='utf-8') as f:
     hotkeys = json.load(f)
 
-browsers = fb.get_installed_browsers()
-default_browser = fb.get_default_browser()
-default_browser_name = default_browser.split('\\')[-1].split('.')[0]
+browsers = {}
+default_browser = ''
+default_browser_name = ''
 
-def run(url, type, arg):
-    if type == 'url' or url.startswith('http'):
+try:
+    # все установленные браузеры
+    browsers = fb.get_installed_browsers()
+    # print(browsers)
+    # браузер, который выбран по умолчанию
+    default_browser = fb.get_default_browser()
+    # имя браузера по умолчанию
+    default_browser_name = default_browser.split('\\')[-1].split('.')[0]
+
+except Exception as e:
+    print(f'Ошибка при проверке браузеров: {e}')
+
+# функция открытия папки/программы/ссылки
+def run(hotkey):
+
+    hotkey_type = hotkey.get('type')
+    # print(hotkey)
+
+    if hotkey_type == 'url':
 
         def open_url():
+            url = hotkey.get('url', [])
+            if isinstance(url, str):
+                url = [url]
+            hotkey_browser = hotkey.get('browser', '')
 
-            if arg == '' or browsers[f'{arg}'] == default_browser:
-                webbrowser.open(url)
-
-            elif default_browser_name == arg:
-                browser = webbrowser.get(browsers[f'{arg}'])
-                browser.open(url)
-
-            elif arg and os.path.exists(browsers[f'{arg}']):
-                print('chrome in browsers')
-                browser = webbrowser.get(browsers[f'{arg}'])
-                browser.open(url)
+            for site_url in url:
+                if not hotkey_browser or hotkey_browser.lower() == default_browser_name.lower():
+                    webbrowser.open(site_url)
+                else:
+                    browser_path = browsers.get(hotkey_browser)
+                    if browser_path and os.path.exists(browser_path):
+                        try:
+                            new_browser = webbrowser.get(browser_path)
+                            new_browser.open(site_url)
+                        except Exception as e:
+                            print(f'Ошибка при открытии браузера {hotkey_browser}: {e}')
+                            webbrowser.open(site_url)
+                    else:
+                        print(f'Ошибка при получении браузера {hotkey_browser}')
+                        webbrowser.open(site_url)
 
         return open_url
 
-    if type == 'program':
+    if hotkey_type == 'program':
 
         def open_program():
-            subprocess.Popen([url, arg])
+            path = hotkey.get('path', [])
+            args = hotkey.get('arg', [])
+            if isinstance(path, str):
+                path = [path]
+            if isinstance(args, str):
+                args = [args]
+            for program_path in path:
+                if not os.path.exists(program_path):
+                    print(f'Файл не найден: {program_path}')
+                    continue
+                # if program_path.split('.')[-1] in ['docx', 'xls', 'xlsx', 'pdf', 'jpeg', 'jpg', 'png']:
+                extension = os.path.splitext(program_path)[1].lower()
+                if extension in ['.docx', '.xls', '.xlsx', '.pdf', '.jpeg', '.jpg', '.png']:
+                    os.startfile(program_path)
+                else:
+                    full_path = [program_path] + args
+                    subprocess.Popen(full_path)
+
         return open_program
 
+    if hotkey_type == 'folder':
 
+        def open_folder():
+            path = hotkey.get('path', [])
+            if isinstance(path, str):
+                path = [path]
+            for folder_path in path:
+                if os.path.exists(folder_path):
+                    # os.startfile(folder_path)
+                    subprocess.Popen(f'explorer "{folder_path}"', shell=True)
+        return open_folder
+
+# основная функция, которая перебирает значения из json
 def main():
 
     for hotkey in hotkeys:
-
-        if isinstance(hotkey['url'], str):
-            urls = []
-            urls.append(hotkey['url'])
-        else:
-            urls = hotkey['url']
-
-        for object in urls:
-            key = hotkey['key']
-            url = object
-            type = hotkey['type']
-            arg = hotkey['arg']
-            keyboard.add_hotkey(key, run(url, type, arg))
+        keyboard.add_hotkey(hotkey['key'], run(hotkey))
 
     try:
         keyboard.wait()
     except KeyboardInterrupt:
         pass
-
 
 if __name__ == "__main__":
     main()
